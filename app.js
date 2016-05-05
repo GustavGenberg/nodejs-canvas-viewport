83d9e5dcff69c90d88a79b245cac33effe6a7f2f
Gustav Genberg <gurra.genberg@gmail.com>
2016-05-05 16:34:49 +0200

    first version done

--- app.js
+++ app.js
@@ -8,13 +8,46 @@ app.listen(2000, function () {
   console.log('Express app listening on port ' + 2000);
 });
 
+var log = function (data) {
+  console.log(data);
+};
+
+var getRandomValue = function (a, b) {
+  return Math.floor(Math.random() * b) + a;
+};
+
+var config = {
+  viewport: {
+    height: 500,
+    width: 500
+  },
+  map: {
+    height: 5000,
+    width: 5000,
+    fps: 1000 / 30,
+    bg: '/bg.jpg'
+  },
+  player: {
+    speed: 10
+  }
+};
 
 
-var playerCount = 0, Players = {};
+var playerCount = 0, Objects = {}, Sockets = {}, Intervals = {};
+Objects.Players = {};
 
 io.on('connect', function (socket) {
   playerCount++;
-  Players[playerCount] = new Player (playerCount, 'Unnamed', 10, 10, socket);
+  Objects.Players[playerCount] = new Player (playerCount, 'Unnamed' + getRandomValue(0, 100), getRandomValue(0, config.map.width), getRandomValue(0, config.map.height), socket);
+
+  log('user connected');
+  socket.emit('config', config);
+
+  socket.on('disconnect', function () {
+    clearInterval(Intervals[playerCount]);
+    delete Sockets[playerCount];
+    delete Objects.Players[playerCount];
+  });
 });
 
 var Player = function (id, nickname, x, y, socket) {
@@ -22,9 +55,51 @@ var Player = function (id, nickname, x, y, socket) {
   this.nickname = nickname;
   this.x = x;
   this.y = y;
-  this.socket = socket;
+  this.viewport = {
+    minx: x - (config.viewport.width / 2),
+    miny: y - (config.viewport.height / 2),
+    maxx: x + (config.viewport.width / 2),
+    maxy: y + (config.viewport.height / 2)
+  };
+  Sockets[id] = socket;
+  this.keysDown = {};
+
+  this.setEmit();
+  this.bindKeys();
 };
 
 Player.prototype = {
+  setEmit: function () {
+    var player = this;
+    Intervals[player.id] = setInterval(function () {
+      if(player.keysDown[37] == true) { // Left
+        player.x = player.x - config.player.speed;
+        player.viewport.minx = player.viewport.minx - config.player.speed;
+        player.viewport.maxx = player.viewport.maxx - config.player.speed;
+      }
+      if(player.keysDown[39] == true) { // Right
+        player.x = player.x + config.player.speed;
+        player.viewport.minx = player.viewport.minx + config.player.speed;
+        player.viewport.maxx = player.viewport.maxx + config.player.speed;
+      }
+      if(player.keysDown[38] == true) { // Up
+        player.y = player.y - config.player.speed;
+        player.viewport.miny = player.viewport.miny - config.player.speed;
+        player.viewport.maxy = player.viewport.maxy - config.player.speed;
+      }
+      if(player.keysDown[40] == true) { // Down
+        player.y = player.y + config.player.speed;
+        player.viewport.miny = player.viewport.miny + config.player.speed;
+        player.viewport.maxy = player.viewport.maxy + config.player.speed;
+      }
 
+      Sockets[player.id].emit('drawData', [Objects, player.viewport]);
+    }, config.map.fps);
+  },
+  bindKeys: function () {
+    var player = this;
+    Sockets[player.id].on('keysDown', function (keysDown) {
+      player.keysDown = keysDown;
+    });
+  }
 };
--- www/game.js
+++ www/game.js
@@ -1,9 +1,102 @@
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+var socket = io(':3000');
+var fps = {fps: {out: 0, count: 0},rfps: {out: 0, count: 0}};
+var drawData = {}, viewport = {};
+var config;
+var cache = {};
+var keysDown = {};
+var image = {};
+
 var init = function () {
-  var canvas = document.createElement('canvas');
-  canvas.width = 500;
-  canvas.height = 500;
-  canvas.style.border = '1px solid red';
-  document.body.appendChild(canvas);
+
+
+
+  cache.viewport = {x: 0, y: 0};
+
+  var getRandomValue = function (a, b) {
+    return Math.floor(Math.random() * b) + a;
+  };
+
+  var log = function (data) {
+    console.log(data);
+  };
+
+  document.addEventListener('keydown', function (event) {
+    keysDown[event.keyCode] = true;
+  });
+  document.addEventListener('keyup', function (event) {
+    delete keysDown[event.keyCode];
+  });
+
+  socket.on('connect', function () {
+
+    socket.on('disconnect', function () {
+
+    });
+
+    socket.on('config', function (data) {
+      config = data;
+
+      canvas.width = config.viewport.width;
+      canvas.height = config.viewport.height;
+      canvas.style.border = '1px solid red';
+      document.body.appendChild(canvas);
+
+      image.background = new Image();
+      image.background.src = config.map.bg;
+
+      image.background.onload = draw;
+    });
+
+  });
+
+  socket.on('drawData', function (data) {
+    fps.rfps.count++;
+    drawData = data[0];
+    viewport = data[1];
+  });
+
+  ctx.globalCompositeOperation='destination-over';
+
+  var draw = function () {
+
+    socket.emit('keysDown', keysDown);
+
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, config.viewport.width, config.viewport.height);
+
+    ctx.translate( -viewport.minx, -viewport.miny );
+
+    for(var i = 0;i*image.background.width < config.map.width;i++) {
+      for(var j = 0;j*image.background.height < config.map.height;j++) {
+        ctx.drawImage(image.background, i*image.background.width, j*image.background.height);
+      }
+    }
+
+    for (player in drawData.Players) {
+      var player = drawData.Players[player];
+      ctx.fillRect(player.x, player.y, 10, 10);
+      ctx.fillStyle = '#FFF';
+      ctx.fillText(player.nickname, player.x, player.y);
+    }
+
+    ctx.fillText('FPS: ' + fps.fps.out, viewport.minx + 10, viewport.miny + 15);
+    ctx.fillText('rFPS: ' + fps.rfps.out, viewport.minx + 6, viewport.miny + 25);
+    ctx.fillText('X, Y: ' + Number(viewport.minx + (config.viewport.width / 2)) + ' : ' + Number(viewport.miny + (config.viewport.height / 2)), viewport.minx + 10, viewport.miny + 35);
+
+    fps.fps.count++;
+    requestAnimationFrame(draw);
+
+  };
+
+  setInterval(function () {
+    fps.fps.out = fps.fps.count;
+    fps.fps.count = 0;
+    fps.rfps.out = fps.rfps.count;
+    fps.rfps.count = 0;
+  }, 1000);
+
 };
 
 $(document).ready(init);
